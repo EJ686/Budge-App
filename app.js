e3a9e4e859f9682022c0768524cc1e65df986efd
Edgar Juarez <ejjt.686@gmail.com>
2017-05-30 17:43:20 -0700

    Added button and return key events

--- app.js
+++ app.js
@@ -17,16 +17,22 @@ let UIController = (function(){
 // GLOBAL APP CONTROLLER
 let controller = (function(budgetCtrl, UICtrl){
 
-    document.querySelector('.add__btn').addEventListener('click', function() {
+    let ctrlAddItem = function() {
         // 1. Get the field input data
         // 2. Add the item to the budget CONTROLLER
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
-    });
+        console.log ('test');
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
-        console.log(event);
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
     });
 
 })(budgetController, UIController);
