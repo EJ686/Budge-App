6004c15871aa395e8cd17ca5c93bfe25b5d1b542
Edgar Juarez <ejjt.686@gmail.com>
2017-05-30 19:34:35 -0700

    Worked on Budget controller and started its data structure

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 // BUDGET CONTROLLER
-let budgetController = (function() {
-    // Some code
+const budgetController = (function() {
+    const Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    const Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    let data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
 
 })();
 
 
 
 // UI CONTROLLER
-let UIController = (function(){
+const UIController = (function(){
 
-    let DOMstrings = {
+    const DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
@@ -34,10 +54,10 @@ let UIController = (function(){
 
 
 // GLOBAL APP CONTROLLER
-let controller = (function(budgetCtrl, UICtrl){
+const controller = (function(budgetCtrl, UICtrl){
 
-    let setupEventListeners = function() {
-        let DOM = UICtrl.getDOMstrings();
+    const setupEventListeners = function() {
+        const DOM = UICtrl.getDOMstrings();
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
         document.addEventListener('keypress', function(event) {
@@ -47,9 +67,9 @@ let controller = (function(budgetCtrl, UICtrl){
         });
     };
 
-    let ctrlAddItem = function() {
+    const ctrlAddItem = function() {
         // 1. Get the field input data
-        let input = UICtrl.getInput();
+        const input = UICtrl.getInput();
         // 2. Add the item to the budget CONTROLLER
         // 3. Add the item to the UI
         // 4. Calculate the budget
